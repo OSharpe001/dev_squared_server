61504ac8bb68f5dade44bd50a6acc4acd0d80a39
O. Sharpe <osharpe001@gmail.com>
2023-10-09 19:22:24 -0400

    set up the default layout for all pages

--- views/layout/Default.jsx
+++ views/layout/Default.jsx
@@ -5,15 +5,15 @@ module.exports = function Default({ children }) {
     <div>
       <html>
         <head>
-          <meta charset="utf-8" />
-          <link rel="icon" href="./assets/favicon.ico" />
+          <meta charSet="utf-8" />
+          {/* <link rel="icon" href="./assets/favicon.ico" /> */}
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta name="theme-color" content="#000000" />
           <meta name="description" content="Dev Squared is a developer's greatest asset for development!" />
           <meta name="og:description" content="Dev Squared is a developer's greatest asset for development!"/>
           <meta name="og:title" content="Dev Squared"/>
           <meta name="og:image" content="./assets/dev_squared.png"/>
-          <link rel="apple-touch-icon" href="./assets/favicon.ico" />
+          {/* <link rel="apple-touch-icon" href="./assets/favicon.ico" /> */}
           <title>Dev Squared</title>
         </head>
         <body>
